b969547fb9b8a100942f029c06e29fa4ac84e77d
Yohannes <y.berhane56@gmail.com>
2026-04-28 22:29:12 -0400

    Parse into LogEntry from Lines Sentinel

--- build.sbt
+++ build.sbt
@@ -14,6 +14,7 @@ lazy val root = (project in file("."))
     ),
 
     libraryDependencies ++= Seq(
+      "org.scala-lang" %% "toolkit" % "0.7.0",
       "org.typelevel" %% "cats-effect" % "3.7.0",
       "org.typelevel" %% "cats-effect-std" % "3.5.3",
       "org.typelevel" %% "munit-cats-effect" % "2.2.0" % Test
--- src/main/scala/log-sentinel/Sentinel.scala
+++ src/main/scala/log-sentinel/Sentinel.scala
@@ -1,9 +1,10 @@
 import cats.syntax.all.*
 import cats.effect.{IO, IOApp, Sync}
 import java.sql.Timestamp
-import java.io.File
 import cats.effect.kernel.Resource
-import java.io.FileInputStream
+import os.Path
+import geny.Generator
+import scala.util.matching.Regex
 
 //  {"timestamp":"2026-04-27T19:55:10Z","level":"INFO","source":"api","message":"user logged in"}
 
@@ -18,6 +19,33 @@ final case class LogEntry(
     source: String,
     message: String
 )
+
+object LogEntry:
+  private val logLinePattern: Regex = """^(\S+)\s+(\S+)\s+(\S+)\s+(.+)$""".r
+
+  def toTimeStamp(s: String): Timestamp =
+    Timestamp.from(java.time.Instant.parse(s))
+
+  def fromFileLine(line: String): IO[LogEntry] =
+    line match
+      case logLinePattern(tsStr, lvlStr, src, msg) =>
+        lvlStr match
+          case "INFO" => IO.pure(
+              LogEntry(toTimeStamp(tsStr), LogLevel.INFO, src, msg)
+            )
+          case "WARN" => IO.pure(
+              LogEntry(toTimeStamp(tsStr), LogLevel.WARN, src, msg)
+            )
+          case "ERROR" => IO.pure(
+              LogEntry(toTimeStamp(tsStr), LogLevel.ERROR, src, msg)
+            )
+          case other => IO.raiseError(
+              new IllegalArgumentException(s"Unknown log level: $other")
+            )
+      case _ =>
+        IO.raiseError(
+          IllegalArgumentException(s"Can't parse log line: $line")
+        )
 
 /** LogSentinel parses text and json files representing logs, and analyzes and
   * summarizes the data. If args is left empty, the program assumes the logs are
@@ -35,21 +63,22 @@ object LogSentinel extends IOApp.Simple:
   def run: IO[Unit] =
     for
       files <- getLogs()
-      _ <- files.traverse_(processFile)
+      logEntries <- files.traverse(getLogEntries)
+      _ <- IO.println(logEntries)
     yield ()
 
-  private def getLogs(): IO[List[File]] =
-    IO.blocking {
-      Option(new File("logs").listFiles()).toList.flatten.filter(_.isFile)
-    }
+  private def getLogs(): IO[List[Path]] = IO.blocking {
+    os.list(os.pwd / "logs").filter(os.isFile).toList
+  }
 
-  def readStream[F[_]: Sync](f: File): Resource[F, FileInputStream] =
-    Resource.make(Sync[F].blocking(new FileInputStream(f))) {
-      inStream =>
-        Sync[F].blocking(inStream.close()).handleErrorWith(_ => Sync[F].unit)
-    }
+  def readStream[F[_]: Sync](f: Path): Resource[F, Generator[String]] =
+    Resource.make(Sync[F].blocking(os.read.lines.stream(f)))(_ => Sync[F].unit)
+    // inStream =>
+    //  Sync[F].blocking(inStream.close()).handleErrorWith(_ => Sync[F].unit)
 
-  def processFile(file: File): IO[Unit] =
-    readStream[IO](file).use { fis =>
-      IO.println(s"opened: ${file.getPath}")
+  def getLogEntries(file: Path): IO[List[LogEntry]] =
+    readStream[IO](file).use { gen =>
+      gen.map(
+        _.trim
+      ).filter(_.nonEmpty).toList.traverse(LogEntry.fromFileLine)
     }
